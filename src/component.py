30922b71ef5b42b23e0332f689e101f57a0c2116
Daniel Borner <daniel.borner@keboola.com>
2025-06-03 22:11:33 +0200

    test artifacts

--- src/component.py
+++ src/component.py
@@ -3,6 +3,8 @@ CrewAI Agentic Flow App main class.
 """
 from datetime import datetime, UTC
 import logging
+import os
+import glob
 
 from keboola.component.base import ComponentBase
 from keboola.component.exceptions import UserException
@@ -16,29 +18,49 @@ class Component(ComponentBase):
         super().__init__()
 
     def run(self):
-        run_time = datetime.now(UTC)
-        run_time_str = run_time.strftime("%Y-%m-%dT%H:%M:%SZ")
+        # run_time = datetime.now(UTC)
+        # run_time_str = run_time.strftime("%Y-%m-%dT%H:%M:%SZ")
 
-        config = Configuration(**self.configuration.parameters)
+        # config = Configuration(**self.configuration.parameters)
         # state = self.get_state_file()
-        new_state = {}
+        # new_state = {}
 
-        logging.info("Initializing CrewAI flow...")
-        crew_ai_flow = CrewAIFlowBuilder(config)
+        #logging.info("Initializing CrewAI flow...")
+        # crew_ai_flow = CrewAIFlowBuilder(config)
 
-        crew = crew_ai_flow.build_crewai_flow(inputs={})
+        # crew = crew_ai_flow.build_crewai_flow(inputs={})
 
-        logging.info("Running CrewAI flow...")
-        result = crew.kickoff()
+        # logging.info("Running CrewAI flow...")
+        # result = crew.kickoff()
 
-        print("\n=== CrewAI Flow Completed ===")
-        print("Final Output:\n", result)
+        # print("\n=== CrewAI Flow Completed ===")
+        # print("Final Output:\n", result)
 
         # Update state
-        new_state["last_successful_run"] = run_time_str
-        self.write_state_file(new_state)
-        logging.info("Component state saved.")
-        logging.info("CrewAI processing completed successfully!")
+        # new_state["last_successful_run"] = run_time_str
+        # self.write_state_file(new_state)
+        # logging.info("Component state saved.")
+        # logging.info("CrewAI processing completed successfully!")
+
+        self.save_artifacts()
+        self.load_artifacts()
+
+    def save_artifacts(self):
+        path = "/data/artifacts/out/current"
+        if not os.path.exists(path):
+            os.makedirs(path)
+        with open("/data/artifacts/out/current/test", "w") as file:
+            file.write("this is my artifact file content")
+        logging.info(f"Saved artifacts to {path}")
+
+    def load_artifacts(self):
+        paths = glob.glob("/data/artifacts/in/runs/*/*")
+        logging.info(f"Found paths: {paths}")
+        path = paths[0]
+        if not os.path.exists(path):
+            os.makedirs(path, exist_ok=True)
+        with open(path, "r") as file:
+            logging.info(file.read())
 
 
 """
